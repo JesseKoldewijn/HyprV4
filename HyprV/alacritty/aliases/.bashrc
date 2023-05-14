402bcb68f868aec5e6e91b2fb15a4e40eedff2e0
JesseKoldewijn <dev@glitchtech.eu>
2023-05-14 20:55:26 +0200

    test: partial script rewrite to use alacritty & paru

---
+++ HyprV/alacritty/aliases/.bashrc
@@ -0,0 +1,10 @@
+# Commands
+    # NodeJS
+    # alias np="npm $*"
+    # alias pn="pnpm $*"
+    # alias yr="yarn $*"
+    # Git
+    alias g="git $*"
+    alias gc="git commit -m $*"
+    # System
+    alias la="ls -la $*"
---
+++ HyprV/alacritty/aliases/config.fish
@@ -0,0 +1,12 @@
+set -U fish_greeting ""
+
+# Commands
+    # NodeJS
+    # alias np="npm $*"
+    # alias pn="pnpm $*"
+    # alias yr="yarn $*"
+    # Git
+    alias g="git $*"
+    alias gc="git commit -m $*"
+    # System
+    alias la="ls -la $*"
